6bc6539871fa3891e017f5c8eaae5593061f6fc0
edlym96 <edlym96@hotmail.com>
2018-09-15 02:15:48 +0800

    Finalise python files

--- files/Multivariate Linear Regression/training_simple.py
+++ files/Multivariate Linear Regression/training_simple.py
@@ -3,30 +3,31 @@ TRAINING SCRIPT
 """
 
 import numpy as np
-from SimpleLinear import multi_linear_model
+from multi_linear import multi_linear_model
 from MSE import calculate_MSE
 from gradient_descent import gradient_descent
-from gradient_descent import regularised_gradient_descent
 from loader import loader
 from normalise import normalise
 from visualize import visualize
 
 #initialise variables
-n_epochs = 50
+n_epochs = 50 #epochs means the number of iterations the training runs through the data set
 np.random.seed(0)
 
 #Generate X and y matrices
 X,y=loader("ex1data2")
-X=normalise(X) #normalise features
+
+#normalise features
+X=normalise(X)
+
 m=y.size #get total number of data samples
 X=np.hstack((np.ones(m)[:, np.newaxis], X)) #add intercept values to the feature array
 n=X.shape[1] #get total number of features
 print(X)
 print(y)
 
-#Generate Weights
-W=np.random.rand(n) #Generate n number of weights
-print("weights are:", W)
+#Generate Random N Weights
+W=np.random.rand(n)
 
 #Initialise Model
 y_hat=multi_linear_model(W,X)
